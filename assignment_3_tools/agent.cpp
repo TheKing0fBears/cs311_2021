6d7264a3faa1a4c5f477c14bbb1c61eb5b8751e6
TheKing0fBears <90875696+TheKing0fBears@users.noreply.github.com>
2021-10-30 20:20:58 -0700

    Update agent.cpp

--- assignment_3_tools/agent.cpp
+++ assignment_3_tools/agent.cpp
@@ -32,24 +32,6 @@ int main(int argc, char** argv)
       				cout << "silent";
       				outf << "silent";
 	      			}
-	      
-  /*  int last_move = 0;
-    std::ofstream outf{ "mymoves.dat" };
-    //cout << "You have entered " << argc
-    //     << " arguments:" << "\n";
- 
-    	
-    //for (int i = 0; i < argc; ++i)
-    //    cout << argv[i] << "\n";
-    //cout << "silent";
-
-    if( strcmp(argv[2], "silent" ) == 0){
-		    cout << "confess";
-		    outf << "confess";
-    }else {
-      cout << "silent";
-      outf << "silent";
-    }
-  */
     return 0;
+}
 }
