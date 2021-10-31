d9bd136efd512459b2582fe18190d1c8d204aa66
TheKing0fBears <90875696+TheKing0fBears@users.noreply.github.com>
2021-10-30 18:42:14 -0700

    Add files via upload

---
+++ assignment_3_tools/agent.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  int last_move = 0;
+  cout<<argc<<" arguments have been entered";
+
+  cout<<"confess"; 
+}
