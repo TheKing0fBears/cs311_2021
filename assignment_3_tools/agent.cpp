24f56841abfa42b27075ab636631e98c60041fd4
TheKing0fBears <90875696+TheKing0fBears@users.noreply.github.com>
2021-10-30 19:30:04 -0700

    Update agent.cpp

--- assignment_3_tools/agent.cpp
+++ assignment_3_tools/agent.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
+
 using namespace std;
-
-int main(int argc,char *argv[])
+  
+int main(int argc, char** argv)
 {
-  int last_move = 0;
-  cout<<argc<<" arguments have been entered";
+    int last_move = 0;
+    std::ofstream outf{ "mymoves.dat" };
+    //cout << "You have entered " << argc
+    //     << " arguments:" << "\n";
+ 
+    	
+    //for (int i = 0; i < argc; ++i)
+    //    cout << argv[i] << "\n";
+    //cout << "silent";
 
-  cout<<"confess";
-  return 0;
-  //bro why
+    if( strcmp(argv[2], "silent" ) == 0){
+		    cout << "confess";
+		    outf << "confess";
+    }else {
+      cout << "silent";
+      outf << "silent";
+    }
+  
+    return 0;
 }
